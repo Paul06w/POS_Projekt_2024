1cb63dbd4cf0959194088a720c4a1815d6f34dc9
paulw <paul.wasle@htl-saalfelden.at>
2024-03-18 11:37:08 +0100

    Changes

--- 240308_Notiz_App_Client/MainWindow.xaml.cs
+++ 240308_Notiz_App_Client/MainWindow.xaml.cs
@@ -21,8 +21,40 @@ namespace _240308_Notiz_App_Client
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        List<Notiz> notizen = new List<Notiz>();
+
+        private void ButtonNeueNotiz_Click(object sender, RoutedEventArgs e)
         {
+            Notiz n = new Notiz();
+            n.Show();
+
+            AddButton(DateTime.Now.ToString("dd. MMM yyyy, HH:mm"));
+
+        }
+
+        private void AddButton(string buttonText)
+        {
+            Button button = new Button
+            {
+                Content = buttonText,
+                Width = 150,
+                Height = 30,
+                Margin = new Thickness(5)
+            };
+
+            // Füge einen Event Handler für den Button-Click hinzu
+            button.Click += ButtonVorhandeneNotiz_Click;
+
+            // Füge den Button zum StackPanel in der ScrollView hinzu
+            WrapPanelNotizen.Children.Add(button);
+        }
+
+        private void ButtonVorhandeneNotiz_Click(object sender, RoutedEventArgs e)
+        {
+            Notiz n = new Notiz();
+            n.Show();
+
+            
 
         }
     }
