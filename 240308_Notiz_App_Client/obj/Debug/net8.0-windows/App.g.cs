949815c89b75d2b6a70af7c082285b806bb47a65
paulw <paul.wasle@htl-saalfelden.at>
2024-05-28 15:12:47 +0200

    WPF Client Änderungen

--- 240308_Notiz_App_Client/obj/Debug/net8.0-windows/App.g.cs
+++ 240308_Notiz_App_Client/obj/Debug/net8.0-windows/App.g.cs
@@ -45,7 +45,7 @@ namespace _240308_Notiz_App_Client {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.1.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.5.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace _240308_Notiz_App_Client {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.1.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.5.0")]
         public static void Main() {
             _240308_Notiz_App_Client.App app = new _240308_Notiz_App_Client.App();
             app.InitializeComponent();
